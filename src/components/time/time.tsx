b259c307466c23c66b015448471107a94abbe09e
Mayor Creator <paulaboagye@outlook.com>
2025-04-04 22:16:49 -0400

    Add geolocation support to Time component for location tracking

--- src/components/time/time.tsx
+++ src/components/time/time.tsx
@@ -6,6 +6,22 @@ interface TimeProps {
 }
 
 export function Time({ greeting, time, location, imageUrl }: TimeProps) {
+  if ("geolocation" in navigator) {
+    console.log("geolocation is available");
+  } else {
+    console.log("geolocation is not available");
+  }
+
+  function success(position: GeolocationPosition) {
+    console.log(position);
+    const { coords } = position;
+    console.log("latitude data: " + coords.latitude);
+    console.log("longitude data: " + coords.longitude);
+  }
+
+  navigator.geolocation.getCurrentPosition(success);
+  navigator.geolocation.watchPosition(success);
+
   return (
     <>
       <div>
